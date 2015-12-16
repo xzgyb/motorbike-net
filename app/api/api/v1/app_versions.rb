531141fcc00b0e87e95667a54a2da015c7f3f6c3
gaoyb <gaoyb@meixing.com>
2015-12-16 14:01:30 +0800

    Add name for upload

--- app/api/api/v1/app_versions.rb
+++ app/api/api/v1/app_versions.rb
@@ -12,30 +12,39 @@ module Api::V1
     end
 
     resource :app_versions do
-      desc 'Get newest app version'
+      desc 'Get newest app version with the specified app name'
+      params do
+        requires :name, type: String, desc: 'app name'
+      end
       get :newest do
-        app_version = AppVersion.newest.first
+        app_version = AppVersion.newest(params[:name]).first
         respond_error!('没有最新版本') unless app_version
         respond_ok(version: app_version.version)
       end
 
-      desc 'Get url with the specified version'
+      desc 'Get url with the specified app name and version'
       params do
+        requires :name, type: String, desc: 'app name'
         requires :version, type: String, desc: 'app version'
       end
       get 'url' do
-        app_version = AppVersion.by_version(params[:version]).first
+        app_version = AppVersion.by_name_version(params[:name],
+                                                 params[:version]).first
+
         respond_error!('没有这个版本的App') unless app_version
         full_url = "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{app_version.app.url}"
         respond_ok(url: full_url)
       end
 
-      desc 'Get changelog with the specified version'
+      desc 'Get changelog with the specified app name and version'
       params do
+        requires :name, type: String, desc: 'app name'
         requires :version, type: String, desc: 'app version'
       end
       get 'changelog' do
-        app_version = AppVersion.by_version(params[:version]).first
+        app_version = AppVersion.by_name_version(params[:name],
+                                                 params[:version]).first
+
         respond_error!('没有这个版本的App') unless app_version
         %({"result":1,"changelog":"#{MarkdownRender.markdown(app_version.changelog)}"}).html_safe
       end
--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -4,19 +4,23 @@ class AppVersion
 
   field :version, type: String
   field :changelog, type: String
+  field :name, type: String
 
   mount_uploader :app, AppUploader
 
   scope :ordered, -> { order_by(:created_at => :desc) }
-  scope :newest, -> { ordered.limit(1) }
-  scope :by_version, ->(version) { AppVersion.where(version: version) }
+  scope :named, ->(name) { where(name: /#{name}/i) }
+  scope :versioned, ->(version) { where(version: version) }
+
+  scope :newest, ->(name) {  named(name).ordered.limit(1) }
+  scope :by_name_version, ->(name, version) { named(name).versioned(version) }
 
   validates :version, presence: true, uniqueness: true
   validates :app, presence: true
 
   class GreatestVersionValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-      newest_app_version = AppVersion.newest.first
+      newest_app_version = AppVersion.newest(record.name).first
       if newest_app_version
         if Versionomy.parse(value) <
             Versionomy.parse(newest_app_version.version)
@@ -27,4 +31,8 @@ class AppVersion
   end
 
   validates :version, greatest_version: true
+
+  before_validation do |app_version|
+    app_version.name = File.basename(app_version.app_url, '.*')
+  end
 end
