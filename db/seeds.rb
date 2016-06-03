0adeeced325ba9750d6908629a5fbf38230f64f6
xzgyb <xzgaoyanbing@163.com>
2016-06-03 13:31:09 +0800

    Create user and oauth application when initialized

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.create!(
+  name:                  ENV['ADMIN_USER_NAME'],
+  password:              ENV['ADMIN_USER_PASSWORD'],
+  password_confirmation: ENV['ADMIN_USER_PASSWORD'],
+  email:                 ENV['ADMIN_USER_EMAIL'],
+  admin:                 true);
+
+Doorkeeper::Application.create!(name:         ENV['OAUTH_APPLICATION_NAME'],
+                                uid:          ENV['OAUTH_APPLICATION_UID'],
+                                secret:       ENV['OAUTH_APPLICATION_SECRET'],
+                                redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+                                owner:        admin_user)
+
+
