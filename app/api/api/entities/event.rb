5e71539bb6fee3d9bb20fb7d6466b5f6a8cd20e5
xzgyb <xzgaoyanbing@163.com>
2016-09-09 11:04:18 +0800

    Add action_id and format time

--- app/api/api/entities/event.rb
+++ app/api/api/entities/event.rb
@@ -1,5 +1,9 @@
 module Api::Entities
   class Event < Grape::Entity
+    IS_ACTION_IF_LAMBDA = lambda do |event, _| 
+      !event.event?
+    end 
+
     expose :id
 
     expose :title do |event, _| 
@@ -22,7 +26,7 @@ module Api::Entities
       event.event? ? event.place : event.actionable.place
     end 
 
-    expose :image_url do |event, _|
+    expose(:image_url) do |event, _|
       if event.activity? || event.take_along_something?
         event.actionable.images.empty? ? ""
                                    : event.actionable.images.first.file.url(:thumb)
@@ -34,9 +38,26 @@ module Api::Entities
         ""
       end
     end
+
+    expose(:action_id, if: IS_ACTION_IF_LAMBDA) do |event, _|
+      event.actionable.id
+    end
     
-    expose :start_at
-    expose :end_at
+    expose(:start_at) do |event, _|
+      if event.start_at.present?
+        event.start_at.strftime("%Y-%m-%d %H:%M:%S")
+      else
+        ""
+      end
+    end
+
+    expose(:end_at) do |event, _|
+      if event.end_at.present?
+        event.end_at.strftime("%Y-%m-%d %H:%M:%S")
+      else
+        ""
+      end
+    end
 
     expose(:type) { |instance, _| ::Event.event_types[instance.event_type] }
 
